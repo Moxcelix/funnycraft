70646fc8cd7739011e788d1fc8a611286865cc17
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-04-12 15:48:21 +0300

    all the same refactoring and some particle physics fixes

--- test/Particles.h
+++ test/Particles.h
@@ -12,55 +12,38 @@
 #include"World.h"
 #include"RigidBox.h"
 
-/// <summary>
-/// класс частицы
-/// </summary>
-class Particles
-{
+class Particles {
 private:
 	float _magnitude = .75f;
 public:
-	struct Particle
-	{
-		Vector3 my_pos;		// позиция частицы
-		Vector3 pos;		// центр
-		World* world;		// указатель на мир
-		Block* block;		// блок
-		RigidBox* body;		// твёрдое тело
+	struct Particle {
+		Vector3 my_pos;
+		Vector3 pos;
+		World* world;
+		Block* block;
+		RigidBox* body;
 
-		float max_time;		// время жзни
+		float max_time;
 
-		float time = 0;					// время
-		float dx = 0, dy = 0, dz = 0;	// приращения координат
-		float hv = 0;					// для свободного падения
-		bool is_dead = false;			// флаг "смерти"
+		float time = 0;
+		float dx = 0, dy = 0, dz = 0;
+		float hv = 0;
+		bool is_dead = false;
 
-		float alpha = 0, beta = 0, gamma = 0;	// углы поворота
-		float velocity = 0.007f;				// скорость
-		float acceleration = 0.25f;				// ускорение
+		float alpha = 0, beta = 0, gamma = 0;
+		float velocity = 0.007f;
+		float acceleration = 0.025f;
 
-		float size = 0.1f;			// размер
-		float g = 9.8f * 0.03f;		// ускорение свободного падения
+		float size = 0.1f;
+		float g = 9.8f * 0.03f;
 
-		ColorSquad squad;			// сквад цветов
-		Block::Direction tdir;		// направление текстуры
+		ColorSquad squad;
+		Block::Direction tdir;
 
-		float tx, ty, tdx, tdy;		// координаты текстур
+		float tx, ty, tdx, tdy;
 
-		/// <summary>
-		/// конструктор
-		/// </summary>
-		/// <param name="block"></param>
-		/// <param name="x"></param>
-		/// <param name="y"></param>
-		/// <param name="z"></param>
-		/// <param name="max_time"></param>
-		/// <param name="world"></param>
-		/// <param name="pos"></param>
-		Particle(Block* block, float x, float y, float z, float max_time, World* world, Vector3 pos)
-		{
+		Particle(Block* block, float x, float y, float z, float max_time, World* world, Vector3 pos) {
 			body = new RigidBox(world, size, size, g, { x + pos.x, y + pos.y, z + pos.z }, RB_USE_COLLISION);
-			// приращение координаты начальные значения
 			dx = x * acceleration;
 			dy = y * acceleration;
 			dz = z * acceleration;
@@ -69,7 +52,6 @@ public:
 			this->block = block;
 			this->pos = pos;
 			this->max_time = max_time;
-			// рандомная часть блока
 			tdir = Block::Direction((rand() % 3) * 2);
 			squad = ColorSquad(block->Color(tdir));
 
@@ -80,40 +62,27 @@ public:
 			tdy = rand() / (float)RAND_MAX * .75f;
 		}
 		
-		~Particle() 
-		{
+		~Particle() {
 			delete body;
 		}
-		/// <summary>
-		/// движение
-		/// </summary>
-		/// <param name="deltaTime"></param>
-		void Move(double deltaTime)
-		{
+
+		void Move(double deltaTime) {
 			time += deltaTime * 0.001f;
 			is_dead = time >= max_time;
-			// приращения координат
+
 			dx -= dx * velocity * deltaTime;
 			dy -= dy * velocity * deltaTime;
 			dz -= dz * velocity * deltaTime;
 
-			if (body->grounded) // рассчёт свободного падения
-				hv = 0;
-			else
-				hv -= body->g * deltaTime;
+			if (body->grounded) hv = 0;
+			else hv -= body->g * deltaTime;
 
-			body->Move(dx, dy, dz + hv * deltaTime); // движение
+			body->Move(dx * deltaTime, dy * deltaTime, dz * deltaTime + hv * deltaTime);
 
-			my_pos = body->pos;	// обновление позиции
+			my_pos = body->pos;
 		}
-		/// <summary>
-		/// рассчёт геометрии
-		/// </summary>
-		/// <param name="vd"></param>
-		/// <param name="ud"></param>
-		void SetMesh(VertexData* vd, UVData* ud)
-		{
-			/// рассчет угла поворота до игрока
+
+		void SetMesh(VertexData* vd, UVData* ud) {
 			float px = world->player->camera.x;
 			float py = world->player->camera.y;
 			float pz = world->player->camera.z;
@@ -126,11 +95,9 @@ public:
 			beta = atan2(b.y, b.x);
 			gamma = atan2(c.y, c.x);
 
-			// установка геометрии
 			float l = world->GetLight(floor(my_pos.x + .5f), floor(my_pos.y + .5f), floor(my_pos.z + .5f));
 
-			if(l == 0)
-				l = world->GetLight(floor(pos.x + .5f), floor(pos.y + .5f), floor(pos.z + .5f));
+			if(l == 0) l = world->GetLight(floor(pos.x + .5f), floor(pos.y + .5f), floor(pos.z + .5f));
 
 			l = (l / float(Chunk::MaxLight)) * (1 - Chunk::brightness) + Chunk::brightness;
 
@@ -143,17 +110,16 @@ public:
 
 	bool working = true;
 
-	World* world;					// указатель на мир
-	VertexData vertexData;			// данные вершин
-	UVData uvData;					// данные UV
-	Vector3 pos;					// позиция
-	vector<Particle*> particles;	// вектор указателей на частицы
+	World* world;
+	VertexData vertexData;
+	UVData uvData;
+	Vector3 pos;
+	vector<Particle*> particles;
 
-	// конструктор
 	Particles(Block* block, World* world, Vector3 pos, int count);
 
-	void Clear();						// очистка
-	void Render(unsigned int texture);	// рендер
-	void Update(double deltaTime);		// обновление
+	void Clear();
+	void Render(unsigned int texture);
+	void Update(double deltaTime);
 };
 
